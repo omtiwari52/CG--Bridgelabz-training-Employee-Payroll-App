35fe7c348f96dbadd204a8b6bc2e22320d4ae828
omtiwari52 <tiwariom900@gmail.com>
2025-02-25 15:57:11 +0530

    [Om Tiwari] add: Use lombok in DTO feature added✨

--- employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/entities/EmployeeEntity.java
+++ employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/entities/EmployeeEntity.java
@@ -2,11 +2,18 @@ package com.bridgelabz.employeepayrollapp.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "employees")
+@Data
 public class EmployeeEntity {
 
+    // Getters and Setters
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -30,36 +37,4 @@ public class EmployeeEntity {
         this.salary = salary;
     }
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public Double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Double salary) {
-        this.salary = salary;
-    }
 }
