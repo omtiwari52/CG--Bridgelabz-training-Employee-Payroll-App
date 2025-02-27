4130478f01b8afe89efb7cb072ab0a839c63892e
omtiwari52 <tiwariom900@gmail.com>
2025-02-27 13:31:56 +0530

    [Om Tiwari] add: Database Configured✨

--- employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/repositories/EmployeeRepository.java
+++ employeepayrollapp/src/main/java/com/bridgelabz/employeepayrollapp/repositories/EmployeeRepository.java
@@ -4,6 +4,7 @@ import com.bridgelabz.employeepayrollapp.entities.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+//using repository to use predefined methods and store data in a database
 @Repository
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
 }
